78cf405ecc989fcf7e60fab664872ae44dd95d5a
Christopher Giffard <christopher.giffard@cgiffard.com>
2012-09-26 23:39:32 +1000

    Made it all work. Still a couple of little tweaks to do.

--- diamondsquare.js
+++ diamondsquare.js
@@ -1,113 +1,136 @@
-function DiamondSquare(data,width,height,roughness) {
-	if (width * height !== data.length)
-		throw Error("Data length mismatch.");
+/*
+	DiamondSquare!
+	http://github.com/cgiffard/DiamondSquare
 	
-	this.dataStore	= data && data.length >= 4 ? data : [0,0,0,0];
-	this.height		= height > 1 ? height : 2;
-	this.width		= width > 1 ? width : 2;
-	this.roughness	= roughness <= 1 ? roughness : 0.1;
-	this.iteration	= 1;
-}
+*/
 
-DiamondSquare.prototype.iterate = function() {
-	var tmpDiamondingArray = [], finalArray = [], x, y, r1, r2, ri;
+(function(glob) {
 	
-	for (x = 0; x < this.width - 1; x ++) {
-		for (y = 0; y < this.height - 1; y ++) {
-			var square = this.getSquare(x,y),
-				sqMax = Math.max.apply(Math,square),
-				sqMin = Math.min.apply(Math,square),
-				sqDif = sqMax - sqMin,
-				interpolatedMedian = sqMin + sqDif/2;
-			
-			if (isNaN(interpolatedMedian)) interpolatedMedian = sqMin;
-			
-			var rough = (sqDif * this.roughness) * (Math.random()-0.5),
-				newHeight = interpolatedMedian + rough;
-			
-			tmpDiamondingArray.push(newHeight);
-		}
+	function DiamondSquare(data,width,height,roughness) {
+		if (width * height !== data.length)
+			throw Error("Data length mismatch.");
+		
+		this.dataStore	= data && data.length >= 4 ? data : [0,0,0,0];
+		this.height		= height > 1 ? height : 2;
+		this.width		= width > 1 ? width : 2;
+		this.roughness	= roughness;
+		this.iteration	= 1;
 	}
 	
-	for (y = 0; y < this.height; y ++) {
-		r1 = this.interpolateRow(y);
+	DiamondSquare.prototype.iterate = function() {
+		var tmpDiamondingArray = [], finalArray = [], x, y,
+			tmpXDoubledGrid = [], tmpDoubledGrid = [];
 		
-		if (this.rowExists(y+1)) {
-			r2 = this.interpolateRow(y+1);
-			ri = this.interpolateRowsVertically(r1,r2,tmpDiamondingArray);
-			finalArray = finalArray.concat(r1,ri);
-		} else {
-			finalArray = finalArray.concat(r1);
+		// Double and interpolate the grid.
+		
+		// Double the width of each row
+		for (var row = 0; row < this.height; row ++) {
+			tmpXDoubledGrid.push(this.interpolateRow(row));
 		}
 		
+		// Now add additional rows by interpolating vertically
+		for (y = 0; y < tmpXDoubledGrid.length -1; y++) {
+			tmpDoubledGrid.push(tmpXDoubledGrid[y]);
+			tmpDoubledGrid.push(
+				this.interpolateRowsVertically(
+						tmpXDoubledGrid[y],
+						tmpXDoubledGrid[y+1]));
+		}
 		
-	}
-	
-	// Save our new width, height, and iteration
-	this.width = (this.width*2) -1;
-	this.height = (this.height*2) -1;
-	this.iteration ++;
-	
-	this.dataStore = finalArray;
-};
-
-DiamondSquare.prototype.getSquare = function(x,y) {
-	var cursor = (y * this.width) + x,
-		firstRow = this.dataStore.slice(cursor,cursor+2),
-		secondRow = this.dataStore.slice(cursor+this.width,cursor+this.width+2);
-	
-	return firstRow.concat(secondRow);
-};
-
-DiamondSquare.prototype.rowExists = function(y) {
-	if (this.dataStore.length > y * this.width) return true;
-	return false;
-};
-
-DiamondSquare.prototype.interpolateRow = function(y) {
-	var cursor = 0, row = [], val1 = 0, val2 = 0;
-	
-	// Interpolate real row...
-	for (x = 0; x < this.width - 1; x ++) {
-		cursor = (y * this.width) + x;
-		val1 = this.dataStore[cursor];
-		val2 = this.dataStore[cursor+1];
-		vali = this.interpolate(val1,val2);
+		// And add the last row back in...
+		tmpDoubledGrid.push(tmpXDoubledGrid[tmpXDoubledGrid.length-1]);
 		
-		row.push(val1,vali);
+		// Now comes the diamond part of the equation.
+		// Calculate for roughness!
+		for (y = 0; y < tmpDoubledGrid.length; y++) {
+			if (y % 2) {
+				var prevRow = tmpDoubledGrid[y-1],
+					nextRow = tmpDoubledGrid[y-1],
+					square = [];
+				tmpDiamondingArray = tmpDoubledGrid[y];
+				for (x = 0; x < tmpDiamondingArray.length; x++) {
+					if (x % 2) {
+						square = [
+							prevRow[x],
+							tmpDiamondingArray[x-1],
+							tmpDiamondingArray[x+1],
+							nextRow[x] ];
+						
+						var sqMax = Math.max.apply(Math,square),
+							sqMin = Math.min.apply(Math,square),
+							sqDif = sqMax - sqMin,
+							interpolatedMedian = sqMin + sqDif/2;
+						
+						var rough = ((sqDif / this.iteration) * this.roughness) * (Math.random()-0.5),
+							newHeight = interpolatedMedian + rough;
+						
+						tmpDiamondingArray[x] = newHeight;
+					}
+				}
+				
+				finalArray = finalArray.concat(tmpDiamondingArray);
+			} else {
+				finalArray = finalArray.concat(tmpDoubledGrid[y]);
+			}
+		}
 		
-		if (x === this.width-2) row.push(val2);
-	}
+		// Save our new width, height, and iteration
+		this.width = (this.width*2) -1;
+		this.height = (this.height*2) -1;
+		this.iteration ++;
+		
+		this.dataStore = finalArray;
+	};
 	
-	return row;
-};
-
-DiamondSquare.prototype.interpolateRowsVertically = function(r1,r2,diamond) {
-	var mixRow = [];
-	for (var rowPointer = 0; rowPointer < r1.length; rowPointer ++) {
+	DiamondSquare.prototype.getSquare = function(x,y) {
+		var cursor = (y * this.width) + x,
+			firstRow = this.dataStore.slice(cursor,cursor+2),
+			secondRow = this.dataStore.slice(cursor+this.width,cursor+this.width+2);
 		
-		if (diamond && rowPointer%2) {
-			mixRow.push(diamond[((rowPointer+1)/2)-1]);
-		} else {
+		return firstRow.concat(secondRow);
+	};
+	
+	DiamondSquare.prototype.interpolateRow = function(y) {
+		var cursor = 0, row = [], val1 = 0, val2 = 0;
+		
+		// Interpolate real row...
+		for (x = 0; x < this.width - 1; x ++) {
+			cursor = (y * this.width) + x;
+			val1 = this.dataStore[cursor];
+			val2 = this.dataStore[cursor+1];
+			vali = this.interpolate(val1,val2);
+			
+			row.push(val1,vali);
+			
+			if (x === this.width-2) row.push(val2);
+		}
+		
+		return row;
+	};
+	
+	DiamondSquare.prototype.interpolateRowsVertically = function(r1,r2) {
+		var mixRow = [];
+		for (var rowPointer = 0; rowPointer < r1.length; rowPointer ++) {
 			mixRow.push(this.interpolate(r1[rowPointer],r2[rowPointer]));
 		}
-	}
+		
+		return mixRow;
+	};
 	
-	return mixRow;
-};
-
-DiamondSquare.prototype.interpolate = function(val1,val2) {
-	var max = Math.max(val1,val2), min = Math.min(val1,val2),
-		result = max !== min ? ((max-min)/2) + min : min;
-	return !isNaN(result) ? result : min;
-};
-
-DiamondSquare.prototype.max = function() {
-	return Math.max.apply(Math,this.dataStore);
-};
-
-DiamondSquare.prototype.min = function() {
-	return Math.min.apply(Math,this.dataStore);
-};
-
-window.DiamondSquare = DiamondSquare;
+	DiamondSquare.prototype.interpolate = function(val1,val2) {
+		var max = Math.max(val1,val2), min = Math.min(val1,val2),
+			result = max !== min ? ((max-min)/2) + min : min;
+		return !isNaN(result) ? result : min;
+	};
+	
+	DiamondSquare.prototype.max = function() {
+		return Math.max.apply(Math,this.dataStore);
+	};
+	
+	DiamondSquare.prototype.min = function() {
+		return Math.min.apply(Math,this.dataStore);
+	};
+	
+	glob.DiamondSquare = DiamondSquare;
+	
+})(this);
